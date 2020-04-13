242a1d0c9c0ba06eca973bf3577124dd01a2b15f
小朱 <919756232@qq.com>
2020-04-13 18:10:10 +0800

    add

--- src/views/app/child/overview.vue
+++ src/views/app/child/overview.vue
@@ -36,7 +36,7 @@ export default {
         rows: []
       },
       loading: false,
-      dataEmpty: false,
+      dataEmpty: false
     };
   },
   methods: {
@@ -50,25 +50,66 @@ export default {
           访问量: res.data.result.requests_total
         });
         this.$set(this.chartData.rows, 1, {
-          页面: "应用数据",
-          访问量: res.data.result.appdata.length
+          页面: "nginx_app_list1",
+          访问量: res.data.result.appdata[0].stub_requests
+        });
+        this.$set(this.chartData.rows, 2, {
+          页面: "nginx_app_list2",
+          访问量: res.data.result.appdata[1].stub_requests
+        });
+        this.$set(this.chartData.rows, 3, {
+          页面: res.data.result.appdata[0].nginx_app_list[0].upstream_server,
+          访问量: 0
+        });
+        this.$set(this.chartData.rows, 4, {
+          页面: res.data.result.appdata[0].nginx_app_list[1].upstream_server,
+          访问量: 0
         });
         this.$set(this.chartSettings.links, 0, {
           source: "请求总数",
-          target: "应用数据",
+          target: "nginx_app_list1"
         });
-        if (res.data.result.appdata.length > 0) {
-          res.data.result.appdata.forEach(function(e, i) {
-            this.$set(this.chartData.rows, i + 2, {
-              页面: "子请求数" + (i+1),
-              访问量: e.stub_requests
-            });
-            this.$set(this.chartSettings.links, i + 1, {
-              source: "应用数据",
-              target: "子请求数" + (i+1)
-            });
-          }, this);
-        }
+        this.$set(this.chartSettings.links, 1, {
+          source: "请求总数",
+          target: "nginx_app_list2"
+        });
+        this.$set(this.chartSettings.links, 2, {
+          source: "nginx_app_list1",
+          target: res.data.result.appdata[0].nginx_app_list[0].upstream_server
+        });
+        this.$set(this.chartSettings.links, 3, {
+          source: "nginx_app_list1",
+          target: res.data.result.appdata[0].nginx_app_list[1].upstream_server
+        });
+        // this.$set(this.chartData.rows, 0, {
+        //   页面: "请求总数",
+        //   访问量: res.data.result.requests_total
+        // });
+        // res.data.result.appdata.forEach(function(e, i) {
+        //   this.$set(this.chartData.rows, i + 1, {
+        //     页面: "应用数据" + i,
+        //     访问量: e.stub_requests
+        //   });
+        //   this.$set(this.chartSettings.links, i, {
+        //     source: "请求总数",
+        //     target: "应用数据" + i
+        //   });
+        //   e.nginx_app_list.forEach(function(v, k) {
+        //     this.$set(this.chartData.rows, k + 3, {
+        //       页面: "nginx_app_list" + k,
+        //       访问量: e.upstream_server
+        //     });
+        //     this.$set(
+        //       this.chartSettings.links,
+        //       i + res.data.result.appdata.length,
+        //       {
+        //         source: "应用数据" + i,
+        //         target: "nginx_app_list" + k
+        //       }
+        //     );
+        //   }, this);
+        // }, this);
+
         this.loading = false;
       } else {
         this.loading = false;
