8fbfa45227157024f86ed7d925225d6102bcf002
zgt <z.gt@qq.com>
2020-05-09 17:51:34 +0800

    兼容ie

--- src/config/index.js
+++ src/config/index.js
@@ -6,6 +6,7 @@ export default {
         //pro: 'https://nc.naccenter.cn:8085'
         pro: `${window.location.protocol}//${window.location.hostname}:8085`
     },
+    proxyUrl: 'http://192.168.1.169:8085'
 
 }
 
